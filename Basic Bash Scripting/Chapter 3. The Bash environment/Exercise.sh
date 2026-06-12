3447e0911a8fd0e0f8034d0249e12e3d361c079c
mdrehan2026 <md.rehan@intozi.io>
2026-06-12 12:41:56 +0000

    execsire 3.1st

--- Basic Bash Scripting/Chapter 3. The Bash environment/Exercise.sh
+++ Basic Bash Scripting/Chapter 3. The Bash environment/Exercise.sh
@@ -7,6 +7,12 @@ var3="Happy Birthday"
 echo "first:$var1 second:$var2 third:$var3"
 #Are these local or global variables? ans = yes
 # remove var3
-
-
+unset var3
+#Can you see the two remaining variables in a new terminal window?
+echo "first:$var1 second:$var2"
+echo "yes"
+echo "third:$var3"
+#Edit /etc/profile so that all users are greeted upon login (test this).?
+#For the root account, set the prompt to something like "Danger!! root is doing stuff in \w", preferably in a bright color such as red or pink or in reverse video mode.?
+#Make sure that newly created users also get a nice personalized prompt which informs them on which system in which directory they are working. Test your changes by adding a new user and logging in as that user.?
 
