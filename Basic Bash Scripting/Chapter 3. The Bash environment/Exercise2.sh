b4503f3a14f2c907bbc01240951c0e4d67ba5ddb
mdrehan2026 <md.rehan@intozi.io>
2026-06-12 12:46:15 +0000

    exercise 2 completed

---
+++ Basic Bash Scripting/Chapter 3. The Bash environment/Exercise2.sh
@@ -0,0 +1,6 @@
+#Write a script in which you assign two integer values to two variables. 
+#The script should calculate the surface of a rectangle which has these proportions. It should be aired with comments and generate elegant output.
+var1="13"
+var2="14"
+Area=$((13*14))
+echo "Area of rectangle is $Area"
