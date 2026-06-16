381a65564851ddf063a1beea456326defc629fbf
mdrehan2026 <md.rehan@intozi.io>
2026-06-16 06:21:51 +0000

    exercise chapter 4

---
+++ Basic Bash Scripting/Chapter 4 regular Expression/Exercise1.sh
@@ -0,0 +1,4 @@
+#Display a list of all the users on your system who log in with the Bash shell as a default.
+#!/bin/bash
+w
+#From the /etc/group directory, display all lines starting with the string "daemon".
