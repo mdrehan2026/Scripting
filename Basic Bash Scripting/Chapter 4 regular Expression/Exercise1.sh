e4f98275ab72b46dc4e55016f3e9a7fab7eba2bb
mdrehan2026 <md.rehan@intozi.io>
2026-06-16 07:22:18 +0000

    exercise quesion

--- Basic Bash Scripting/Chapter 4 regular Expression/Exercise1.sh
+++ Basic Bash Scripting/Chapter 4 regular Expression/Exercise1.sh
@@ -3,5 +3,24 @@
 w
 echo
 #From the /etc/group directory, display all lines starting with the string "daemon"
+echo
 grep ^daemon /etc/group 
-
+#Print all the lines from the same file that don't contain the string.
+echo 
+grep -v daemon /etc/group
+#Display localhost information from the /etc/hosts file, display the line number(s) matching the search string 
+# and count the number of occurrences of the string.
+echo
+grep -n localhost /etc/host
+grep -c localhost /etc/host
+#Display a list of /usr/share/doc subdirectories containing information about shells.
+echo 
+# ls -R /usr/shar/doc , correct but too much info
+#Since shell names usually end in "sh" (bash, zsh, dash), searching for "sh" is a great start.
+ls /usr/share/doc | grep "sh"
+#How many README files do these subdirectories contain? Don't count anything in the form of "README.a_string"
+echo 
+ls /usr/share/doc | grep -c "README" 
+# Make a list of files in your home directory that were changed less that 10 hours ago, using grep, 
+# but leave out directories.
+ls /home | grep 
