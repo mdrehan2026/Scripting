965e2e4ffde156ec926a48fc22b985058d858247
mdrehan2026 <md.rehan@intozi.io>
2026-06-16 06:26:08 +0000

    2nd question

--- Basic Bash Scripting/Chapter 4 regular Expression/Exercise1.sh
+++ Basic Bash Scripting/Chapter 4 regular Expression/Exercise1.sh
@@ -1,4 +1,7 @@
 #Display a list of all the users on your system who log in with the Bash shell as a default.
 #!/bin/bash
 w
-#From the /etc/group directory, display all lines starting with the string "daemon".
+echo
+#From the /etc/group directory, display all lines starting with the string "daemon"
+grep ^daemon /etc/group 
+
